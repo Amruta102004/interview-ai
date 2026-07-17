866a010744677916b93bfa4fa26757e3dc3f9483
Amruta Tekale <atekale046@gmail.com>
2026-07-17 20:24:42 +0530

    Add backend health route

--- Backend/server.js
+++ Backend/server.js
@@ -8,7 +8,12 @@ const connectToDB = require("./src/config/database");
 
 connectToDB();
 
-
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Interview AI Backend is running 🚀"
+  });
+});
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
